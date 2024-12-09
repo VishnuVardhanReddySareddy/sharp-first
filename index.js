ce80d72fc3cc5e97b683b018ca48ddc0b6d850df
Vishnu <vishnureddy0010@gmail.com>
2024-12-09 09:37:55 +0530

    added gelElementById feature

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function myFunction() {
-  console.log("THis is javascript");
-}
+// Write your code below:
+
+const header = document.getElementById("main-heading");
+header.textContent = "Fruit World";
+
+header.style.color = "orange";
+
+const header1 = document.getElementById("header");
+
+header1.style.backgroundColor = "green";
+header1.style.borderBottom = "orange";
+
+const header2 = document.getElementById("basket-heading");
+
+header2.style.color = "green";
+
+const header3 = document.getElementById("thanks");
+
+let para = document.createElement("p");
+para.textContent = "Please visit us again";
+
+header3.appendChild(para);
